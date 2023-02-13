4d2892a4d8a0a0dff14dd50ee8d741b117ec7d15
codebasky <123750249+codebasky@users.noreply.github.com>
2023-02-13 07:56:58 +0530

    formatting the quiz package (#10)

--- quiz/cmd/main.go
+++ quiz/cmd/main.go
@@ -12,10 +12,10 @@ var (
 )
 
 func main() {
-	var nFlag = flag.Int("n", 10, "Number of seconds to wait for user input")
-	var fName = flag.String("f", DefaultQuizFile, "Absolute path of the quiz file")
+	var waitTime = flag.Int("w", 10, "Number of seconds to wait for user input")
+	var fileName = flag.String("f", DefaultQuizFile, "Absolute path of the quiz file")
 	flag.Parse()
-	err := quiz.Quiz(*fName, time.Duration(*nFlag)*time.Second)
+	err := quiz.Quiz(*fileName, time.Duration(*waitTime)*time.Second)
 	if err != nil {
 		fmt.Printf("Quiz terminated with error %s", err)
 	}
